78646512db152cdf5c659930057dd4d5ee521d6c
Michael C. Herrera <161962992+michaelcherrera84@users.noreply.github.com>
2025-07-27 16:01:20 -0500

    Add contact section

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -3,10 +3,12 @@ import NavBar from './NavBar';
 import About from './About';
 import Projects from './projects/Projects';
 import Contact from './Contact';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
     return (
         <>
+            <Toaster position="top-right" />
             <NavBar />
             <Banner />
             <About />
