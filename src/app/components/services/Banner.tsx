755c839ec5bbf0ca655f55483714d4075830c1da
Michael C. Herrera <161962992+michaelcherrera84@users.noreply.github.com>
2025-07-29 16:19:59 -0500

    Update tutoring banner and contact placeholder text

--- src/app/components/services/Banner.tsx
+++ src/app/components/services/Banner.tsx
@@ -2,7 +2,7 @@ function Banner() {
     return (
         <div id="home" className="mt-[-50px] pt-[50px] bg-[#343944]">
             <div className="px-4 py-8 flex justify-center items-center lg:px-24">
-                <div className="bg-banner-radial-red text-[#343944] flex flex-col items-center w-full md:w-1/2 p-3 border-6 border-white">
+                <div className="bg-banner-radial-red text-[#343944] flex flex-col items-center w-full md:w-1/2 p-3 border-6 border-white rounded-lg ">
                     <h1 className="text-5xl font-black">Tutoring</h1>
                     <h2 className="text-lg font-semibold">Let me help you succeed!</h2>
                     <img src="/images/tutoring.png" alt="Tutoring" className="h-[282px] w-[235px] mt-3"/>
