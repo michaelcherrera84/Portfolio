7d6ae2ad149190257c30e584ad31ef6399669718
Michael C. Herrera <161962992+michaelcherrera84@users.noreply.github.com>
2025-07-28 22:52:29 -0500

    Fix toast

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -2,14 +2,12 @@ import Banner from '../components/home/Banner';
 import About from '../components/home/About';
 import Projects from '../components/home/projects/Projects';
 import Contact from '../components/home/contact/Contact';
-import { Toaster } from 'react-hot-toast';
 import Footer from '../components/home/Footer';
 import NavBar from '../components/NavBar';
 
 function HomePage() {
     return (
         <>
-            <Toaster position="bottom-right" />
             <NavBar />
             <Banner />
             <About />
