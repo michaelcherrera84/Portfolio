c3f36c99da16079916e2e2b5f0d1791ed2ed3661
michaelcherrera <michael.c.herrera@icloud.com>
2025-08-06 22:40:47 -0500

    Update Project section to fix images sizes for smaller screens.

--- src/app/components/home/projects/Project.tsx
+++ src/app/components/home/projects/Project.tsx
@@ -12,14 +12,13 @@ function fieldset({name, description, imageUrls}: Props) {
         <fieldset className="border p-3.5 rounded-sm">
             <legend className="font-bold">{name}</legend>
             <p className="pb-3.5">{description}</p>
-            <div className="grid grid-cols-1 justify-items-center md:grid md:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3">
                 {imageUrls?.map((url, i) => (
                     <LazyLoadImage
                         key={i}
                         src={url}
                         alt="project screenshot"
-                        width={450}
-                        className="rounded-sm"
+                        className="rounded-sm w-full max-w-[450px] h-auto"
                         placeholderSrc="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCA0MCA0MCI+PHBhdGggZmlsbD0ibm9uZSIgc3Ryb2tlPSIjY2NjIiBzdHJva2Utd2lkdGg9IjQiIGQ9Ik0yMCA0YTE2IDE2IDAgMSAwIDE2IDE2IiBzdHJva2UtZGFzaGFycmF5PSI1LjI3MjczOTczNzMzMDYzNzIsNS4yNzI3Mzk3MzczMzA2MzczIiBzdHJva2UtZGFzaG9mZnNldD0iMCIgLz48L3N2Zz4="
                     />
                 ))}
