2a8fcf1beb21160112cd3d6c2923c170c8dea471
michaelcherrera <michael.c.herrera@icloud.com>
2025-08-06 22:11:43 -0500

    Update projects section with lazy load images

--- src/app/components/home/projects/Project.tsx
+++ src/app/components/home/projects/Project.tsx
@@ -1,3 +1,5 @@
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 type Props = {
     url?: string;
     name: string;
@@ -5,30 +7,37 @@ type Props = {
     imageUrls?: string[];
 }
 
-function fieldset({ name, description, imageUrls }: Props) {
+function fieldset({name, description, imageUrls}: Props) {
     return (
         <fieldset className="border p-3.5 rounded-sm">
             <legend className="font-bold">{name}</legend>
             <p className="pb-3.5">{description}</p>
             <div className="grid grid-cols-1 justify-items-center md:grid md:grid-cols-2 lg:grid-cols-3">
                 {imageUrls?.map((url, i) => (
-                    <img key={i} src={url} alt="project screenshot" width="450" className="rounded-sm"/>
+                    <LazyLoadImage
+                        key={i}
+                        src={url}
+                        alt="project screenshot"
+                        width={450}
+                        className="rounded-sm"
+                        placeholderSrc="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCA0MCA0MCI+PHBhdGggZmlsbD0ibm9uZSIgc3Ryb2tlPSIjY2NjIiBzdHJva2Utd2lkdGg9IjQiIGQ9Ik0yMCA0YTE2IDE2IDAgMSAwIDE2IDE2IiBzdHJva2UtZGFzaGFycmF5PSI1LjI3MjczOTczNzMzMDYzNzIsNS4yNzI3Mzk3MzczMzA2MzczIiBzdHJva2UtZGFzaG9mZnNldD0iMCIgLz48L3N2Zz4="
+                    />
                 ))}
             </div>
         </fieldset>
     );
 }
 
-function Project({ url, name, description, imageUrls }: Props) {
+function Project({url, name, description, imageUrls}: Props) {
     return (
         <div className="py-3.5">
             {url ? (
                 <a href={url} target="_blank" title={url.substring(8)}>
-                    {fieldset({ name, description, imageUrls })}
+                    {fieldset({name, description, imageUrls})}
                 </a>
             ) : (
                 <>
-                    {fieldset({ name, description, imageUrls })}\
+                    {fieldset({name, description, imageUrls})}
                 </>
             )
             }
